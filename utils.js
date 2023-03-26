a5870e097c99eb0d0a251055f9c280d3986a1507
Islom Abdurakhmonov <islomabdurakhmonov8@gmail.com>
2023-03-26 13:23:10 +0500

    updated

---
+++ utils.js
@@ -0,0 +1,26 @@
+function generateProblem () {
+  const items = ['+', '-']
+
+  let first_number = Math.floor(Math.random() * 50)
+  let second_number = Math.floor(Math.random() * 11 + 1)
+  let operator = items[Math.floor(Math.random()*items.length)]
+
+  return `${first_number} ${operator} ${second_number} = ?`
+}
+
+function generateRandomNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function random(obj = {}) {
+  const { max, min } = obj;
+
+  Math.floor(Math.random() * (max + 1)) + min;
+}
+
+
+module.exports = {
+  generateProblem,
+  generateRandomNumber
+}
